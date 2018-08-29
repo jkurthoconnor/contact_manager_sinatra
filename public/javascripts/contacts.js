e0a5c4210f2745dac690096cce35a889049c1fcb
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-28 21:20:42 -0600

    add list template; basic slide effect

--- public/javascripts/contacts.js
+++ public/javascripts/contacts.js
@@ -1,5 +1,13 @@
 
 $(function() {
+  let contactList
+  let $contactsUL = $('.contacts-list');
+  let $noContactsBox = $('.zero-contacts');
+
+  let contactsTemplate = $('#contacts-li').html();
+  let contactsListScript = Handlebars.compile(contactsTemplate);
+
+  getContacts();
 
   function processFormData(serializedArray) {
     let formDataObj = {};
@@ -12,40 +20,47 @@ $(function() {
   }
 
 
+  function drawContactsList(jsonObj) {
+    let contactList = jsonObj;
+    let contactsHTML = contactsListScript({contacts: contactList });
+
+    if (contactList.length > 0) {
+      $contactsUL.append(contactsHTML).show();
+      $noContactsBox.hide();
+    }
+  }
+
   function getContacts() {
     $.ajax({
       url: 'http://localhost:4567/api/contacts', 
       type: 'GET',
       dataType: 'json',
       success: function(json) {
-        console.log(json); // save json return in a variable
+        drawContactsList(json);
       }
     });
   }
 
   function addContact(formObj) {
-
     $.ajax({
       url: 'http://localhost:4567/api/contacts', 
       data: formObj,
       type: 'POST',
       dataType: 'json',
-      success: function(json) {
-        console.log(json);
-      }
     });
   }
 
-    function searchContacts(searchStr, contactList) {
-      let pattern = new RegExp('^' + searchStr, 'i');
+  function searchContacts(searchStr, contactList) {
+    let pattern = new RegExp('^' + searchStr, 'i');
 
-      return contactList.filter(function(contact) {
-        return contact.full_name.match(pattern);
+    return contactList.filter(function(contact) {
+      return contact.full_name.split(/\s+/).some(function(namePart) {
+        return namePart.match(pattern);
       });
-    }
+    });
+  }
    
   function editContact(formObj, id) {
-
     $.ajax({
       url: `http://localhost:4567/api/contacts/${id}`,
       data: formObj,
@@ -57,13 +72,30 @@ $(function() {
     });
   }
 
+  function deleteContact(id) {
+    $.ajax({
+      url: `http://localhost:4567/api/contacts/${id}`,
+      type: 'DELETE',
+      dataType: 'json',
+    });
+  }
+
+  $('.search-add-bar > a').on('click', function(e) {
+    $('.search-add-bar').slideUp();
+    $('.main-panel').slideUp();
+    $('.message').slideUp();
+    $('.form-box').slideDown();
+     
+
+
+  });
+
   $('form').on('submit', function (e) {
     e.preventDefault();
     let  data = $(this).serializeArray();
     let jsonReady = processFormData(data);
     addContact(jsonReady); 
+    getContacts();
   });
-
-
 });
 
