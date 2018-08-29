ed18e85aa4fe929d1f7966bfbfce888e0a9f19d3
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-29 16:28:28 -0600

    implement edit and delete contact

--- public/javascripts/contacts.js
+++ public/javascripts/contacts.js
@@ -1,11 +1,15 @@
 
 $(function() {
-  let contactList
+  let contactList;
   let $contactsUL = $('.contacts-list');
   let $noContactsBox = $('.zero-contacts');
+  let $editContactBox = $('.edit-form-box');
 
   let contactsTemplate = $('#contacts-li').html();
   let contactsListScript = Handlebars.compile(contactsTemplate);
+
+  let editContactTemplate = $('#edit-contact-form').html();
+  let editContactScript = Handlebars.compile(editContactTemplate);
 
   getContacts();
 
@@ -21,11 +25,11 @@ $(function() {
 
 
   function drawContactsList(jsonObj) {
-    let contactList = jsonObj;
+    contactList = jsonObj;
     let contactsHTML = contactsListScript({contacts: contactList });
 
     if (contactList.length > 0) {
-      $contactsUL.append(contactsHTML).show();
+      $contactsUL.html(contactsHTML).show();
       $noContactsBox.hide();
     }
   }
@@ -47,6 +51,11 @@ $(function() {
       data: formObj,
       type: 'POST',
       dataType: 'json',
+      success: function() {
+        $('.form-box').slideUp();
+        $('.search-add-bar').slideDown();
+        $('.main-panel').slideDown();
+      },
     });
   }
 
@@ -68,6 +77,9 @@ $(function() {
       dataType: 'json',
       success: function(json) {
         console.log(json);
+        $('.edit-form-box').empty();
+        $('.search-add-bar').slideDown();
+        $('.main-panel').slideDown();
       }
     });
   }
@@ -85,17 +97,56 @@ $(function() {
     $('.main-panel').slideUp();
     $('.message').slideUp();
     $('.form-box').slideDown();
-     
-
-
   });
 
-  $('form').on('submit', function (e) {
+  $('.contact-form').on('submit', function (e) {
     e.preventDefault();
     let  data = $(this).serializeArray();
     let jsonReady = processFormData(data);
+
     addContact(jsonReady); 
+    this.reset();  // place in `success` in addContact to allow error handling?
     getContacts();
   });
+
+
+  $('.edit-form-box').on('submit', '.edit-form', function (e) {
+    e.preventDefault();
+    let id = $(this).attr('data-contact');
+    let  data = $(this).serializeArray();
+    let jsonReady = processFormData(data);
+    
+    editContact(jsonReady, id);
+    getContacts();
+  });
+
+
+
+  $('.contacts-list').on('click', 'a[data-role="delete"]', function(e) {
+    e.preventDefault();
+    let id = $(this).attr('data-contact');
+
+    deleteContact(id);
+    getContacts();
+  });
+
+  $('.contacts-list').on('click', 'a[data-role="edit"]', function(e) {
+    e.preventDefault();
+    let id = $(this).attr('data-contact');
+    let contact =  contactList.filter(function(contact) {
+      return contact.id === Number(id);
+    })[0];
+
+    console.log(editContactTemplate);
+    let editHTML = editContactScript(contact); 
+    $editContactBox.html(editHTML);
+
+    $('.search-add-bar').slideUp(); // extract: also used for show add contact
+    $('.main-panel').slideUp();     //
+    $('.message').slideUp();        //
+    $('.edit-form-box').slideDown(); // unique; provide `down` argument
+  });
+
+
 });
 
