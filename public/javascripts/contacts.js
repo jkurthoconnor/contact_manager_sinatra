281fbc1a7d78d5653b16675bcad7ed232a409880
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-29 17:39:25 -0600

    refactor: consolidate form boxes into one

--- public/javascripts/contacts.js
+++ public/javascripts/contacts.js
@@ -3,10 +3,12 @@ $(function() {
   let contactList;
   let $contactsUL = $('.contacts-list');
   let $noContactsBox = $('.zero-contacts');
-  let $editContactBox = $('.edit-form-box');
+  let $editContactBox = $('.form-box');
 
   let contactsTemplate = $('#contacts-li').html();
   let contactsListScript = Handlebars.compile(contactsTemplate);
+
+  let addContactTemplate = $('#add-contact-form').html();
 
   let editContactTemplate = $('#edit-contact-form').html();
   let editContactScript = Handlebars.compile(editContactTemplate);
@@ -52,9 +54,11 @@ $(function() {
       type: 'POST',
       dataType: 'json',
       success: function() {
+        getContacts();
         $('.form-box').slideUp();
         $('.search-add-bar').slideDown();
         $('.main-panel').slideDown();
+        $('.form-box').empty();
       },
     });
   }
@@ -76,10 +80,11 @@ $(function() {
       type: 'PUT',
       dataType: 'json',
       success: function(json) {
-        console.log(json);
-        $('.edit-form-box').empty();
+        getContacts();
+        $('.form-box').slideUp();
         $('.search-add-bar').slideDown();
         $('.main-panel').slideDown();
+        $('.form-box').empty();
       }
     });
   }
@@ -89,45 +94,44 @@ $(function() {
       url: `http://localhost:4567/api/contacts/${id}`,
       type: 'DELETE',
       dataType: 'json',
+      success: function() {
+        getContacts();
+      }
     });
   }
 
   $('.search-add-bar > a').on('click', function(e) {
+    console.log(addContactTemplate);  
+    $('.form-box').html(addContactTemplate);
     $('.search-add-bar').slideUp();
     $('.main-panel').slideUp();
     $('.message').slideUp();
     $('.form-box').slideDown();
   });
 
-  $('.contact-form').on('submit', function (e) {
+  $('.form-box').on('submit', '#add', function (e) {
     e.preventDefault();
     let  data = $(this).serializeArray();
     let jsonReady = processFormData(data);
 
     addContact(jsonReady); 
-    this.reset();  // place in `success` in addContact to allow error handling?
-    getContacts();
   });
 
 
-  $('.edit-form-box').on('submit', '.edit-form', function (e) {
+  $('.form-box').on('submit', '#edit-form', function (e) {
     e.preventDefault();
     let id = $(this).attr('data-contact');
     let  data = $(this).serializeArray();
     let jsonReady = processFormData(data);
     
     editContact(jsonReady, id);
-    getContacts();
   });
-
-
 
   $('.contacts-list').on('click', 'a[data-role="delete"]', function(e) {
     e.preventDefault();
     let id = $(this).attr('data-contact');
 
     deleteContact(id);
-    getContacts();
   });
 
   $('.contacts-list').on('click', 'a[data-role="edit"]', function(e) {
@@ -137,14 +141,13 @@ $(function() {
       return contact.id === Number(id);
     })[0];
 
-    console.log(editContactTemplate);
     let editHTML = editContactScript(contact); 
     $editContactBox.html(editHTML);
 
     $('.search-add-bar').slideUp(); // extract: also used for show add contact
     $('.main-panel').slideUp();     //
     $('.message').slideUp();        //
-    $('.edit-form-box').slideDown(); // unique; provide `down` argument
+    $('.form-box').slideDown(); // unique; provide `down` argument
   });
 
 
