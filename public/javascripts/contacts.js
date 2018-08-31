51ef2c256917485afb7627a4c072d9ff2df44d84
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-30 18:46:38 -0600

    refactor: extract ui, api, helper objects

--- public/javascripts/contacts.js
+++ public/javascripts/contacts.js
@@ -1,4 +1,3 @@
-
 $(function() {
   let contactList;
   let $contactsUL = $('.contacts-list');
@@ -14,145 +13,158 @@ $(function() {
   let editContactTemplate = $('#edit-contact-form').html();
   let editContactScript = Handlebars.compile(editContactTemplate);
 
-  getContacts();
-
-  function processFormData(serializedArray) {
-    let formDataObj = {};
-
-    serializedArray.forEach(function(field) {
-      formDataObj[field.name] = field.value;
-    });
-
-    return formDataObj;
-  }
-
-  function showMain() {
-    $formBox.slideUp();
-    $('#search-bar').slideDown();
-    $('.main-panel').slideDown();
-    $formBox.empty();
-  }
-
-  function showForm(markup) {
-    $formBox.html(markup);
-    $('#search-bar').slideUp();
-    $('.main-panel').slideUp();
-    $('.message').slideUp();
-    $formBox.slideDown();
-  }
-
-  function showMessage() {
-    $('.contacts-list').slideUp();
-    $('.message').show();
-  }
-
-  function hideMessage() {
-    $('.message').hide();
-  }
-
-  function drawContactsList(jsonObj) {
-    let contactsHTML = contactsListScript({contacts: jsonObj });
-
-    if (jsonObj.length > 0) {
-      $contactsUL.html(contactsHTML).show();
-      $noContactsBox.hide();
-    } else {
-      $contactsUL.hide();
-      $noContactsBox.show();
-    }
-  }
-
-  function getContacts() {
-    $.ajax({
-      url: 'http://localhost:4567/api/contacts', 
-      type: 'GET',
-      dataType: 'json',
-      success: function(json) {
-        splitTags(json);
-        contactList = json;
-        drawContactsList(json);
-      }
-    });
-  }
-
-  function splitTags(contactsJson) {
-    contactsJson.forEach(function(contact, idx, arr) {
-      if (contact.tags) {
-        arr[idx].tags = contact.tags.split(',');
-      }
-    });
-  }
-
-  function addContact(formObj) {
-    $.ajax({
-      url: 'http://localhost:4567/api/contacts', 
-      data: formObj,
-      type: 'POST',
-      dataType: 'json',
-      success: function() {
-        getContacts();
-        showMain();
-      },
-    });
-  }
-
-  function searchTags(searchTag, contacts) {
-    return contacts.filter(function(contact) {
-      if (contact.tags) {
-        return contact.tags.includes(searchTag);
-      }
-    })
-  }
-
-  function searchNames(searchStr, contacts) {
-    let pattern = new RegExp('^' + searchStr, 'i');
-
-    return contacts.filter(function(contact) {
-      return contact.full_name.split(/\s+/).some(function(namePart) {
-        return namePart.match(pattern);
+  const api = {
+    getContacts: function() {
+      $.ajax({
+        url: 'http://localhost:4567/api/contacts', 
+        type: 'GET',
+        dataType: 'json',
+        success: function(json) {
+          helpers.splitTags(json);
+          contactList = json;
+          ui.drawContactsList(json);
+        }
       });
-    });
-  }
+    },
 
-  function editContact(formObj, id) {
-    $.ajax({
-      url: `http://localhost:4567/api/contacts/${id}`,
-      data: formObj,
-      type: 'PUT',
-      dataType: 'json',
-      success: function(json) {
-        getContacts();
-        showMain();
-      }
-    });
-  }
+    addContact: function(formObj) {
+      let self = this;
 
-  function deleteContact(id) {
-    $.ajax({
-      url: `http://localhost:4567/api/contacts/${id}`,
-      type: 'DELETE',
-      dataType: 'json',
-      success: function() {
-        getContacts();
+      $.ajax({
+        url: 'http://localhost:4567/api/contacts', 
+        data: formObj,
+        type: 'POST',
+        dataType: 'json',
+        success: function() {
+          self.getContacts();
+          ui.showMain();
+        },
+      });
+    },
+
+    editContact: function(formObj, id) {
+      let self = this;
+
+      $.ajax({
+        url: `http://localhost:4567/api/contacts/${id}`,
+        data: formObj,
+        type: 'PUT',
+        dataType: 'json',
+        success: function(json) {
+          self.getContacts();
+          ui.showMain();
+        }
+      });
+    },
+
+    deleteContact: function(id) {
+      let self = this;
+
+      $.ajax({
+        url: `http://localhost:4567/api/contacts/${id}`,
+        type: 'DELETE',
+        dataType: 'json',
+        success: function() {
+          self.getContacts();
+        }
+      });
+    },
+  };
+
+
+  const helpers = {
+    processFormData: function(serializedArray) {
+      let formDataObj = {};
+
+      serializedArray.forEach(function(field) {
+        formDataObj[field.name] = field.value;
+      });
+
+      return formDataObj;
+    },
+
+    splitTags: function(contactsJson) {
+      contactsJson.forEach(function(contact, idx, arr) {
+        if (contact.tags) {
+          arr[idx].tags = contact.tags.split(',');
+        }
+      });
+    },
+
+    searchTags: function(searchTag, contacts) {
+      return contacts.filter(function(contact) {
+        if (contact.tags) {
+          return contact.tags.includes(searchTag);
+        }
+      })
+    },
+
+    searchNames: function(searchStr, contacts) {
+      let pattern = new RegExp('^' + searchStr, 'i');
+
+      return contacts.filter(function(contact) {
+        return contact.full_name.split(/\s+/).some(function(namePart) {
+          return namePart.match(pattern);
+        });
+      });
+    },
+  };
+
+  const ui = {
+    showMain: function() {
+      $formBox.slideUp();
+      $('#search-bar').slideDown();
+      $('.main-panel').slideDown();
+      $formBox.empty();
+    },
+
+    showForm: function(markup) {
+      $formBox.html(markup);
+      $('#search-bar').slideUp();
+      $('.main-panel').slideUp();
+      $('.message').slideUp();
+      $formBox.slideDown();
+    },
+
+    showMessage: function() {
+      $('.contacts-list').slideUp();
+      $('.message').show();
+    },
+
+    hideMessage: function() {
+      $('.message').hide();
+    },
+
+    drawContactsList: function(jsonObj) {
+      let contactsHTML = contactsListScript({contacts: jsonObj });
+
+      if (jsonObj.length > 0) {
+        $contactsUL.html(contactsHTML).show();
+        $noContactsBox.hide();
+      } else {
+        $contactsUL.hide();
+        $noContactsBox.show();
       }
-    });
-  }
+    },
+  };
+
+  api.getContacts();
 
   $('.add-contact-button').on('click', function(e) {
-    showForm(addContactTemplate);
+    ui.showForm(addContactTemplate);
   });
 
   $search.on('keyup', function(e) {
     let searchString = $search.val();
-    let matchingContacts = searchNames(searchString, contactList);
+    let matchingContacts = helpers.searchNames(searchString, contactList);
 
-    hideMessage();
+    ui.hideMessage();
 
     if (matchingContacts.length > 0) {
-      drawContactsList(matchingContacts);
+      ui.drawContactsList(matchingContacts);
     } else {
       $('#search-display > span').text(searchString);
-      showMessage();
+      ui.showMessage();
     }
   });
 
@@ -166,35 +178,34 @@ $(function() {
       return;
     } else {
       let data = $(this).serializeArray();
-      let jsonReady = processFormData(data);
+      let jsonReady = helpers.processFormData(data);
 
       $nameField.removeClass('error-message');
-      addContact(jsonReady); 
+      api.addContact(jsonReady); 
     }
   });
-
 
   $('.contacts-list').on('click', 'a[data-tag]', function(e) {
     e.preventDefault();
     let tag = $(this).attr('data-tag');
-    let matchingContacts = searchTags(tag, contactList);
+    let matchingContacts = helpers.searchTags(tag, contactList);
 
-    hideMessage();
-    drawContactsList(matchingContacts);
+    ui.hideMessage();
+    ui.drawContactsList(matchingContacts);
   });
 
   $formBox.on('submit', '#edit-form', function (e) {
     e.preventDefault();
     let id = $(this).attr('data-contact');
     let  data = $(this).serializeArray();
-    let jsonReady = processFormData(data);
+    let jsonReady = helpers.processFormData(data);
     
-    editContact(jsonReady, id);
+    api.editContact(jsonReady, id);
   });
 
   $formBox.on('click', '#cancel', function(e) {
     e.preventDefault();
-    showMain();
+    ui.showMain();
   });
 
   $('.contacts-list').on('click', 'a[data-role="delete"]', function(e) {
@@ -202,7 +213,7 @@ $(function() {
     let id = $(this).attr('data-contact');
 
     if (window.confirm('Do you want to delete this contact?')) {
-      deleteContact(id);
+      api.deleteContact(id);
     }
   });
 
@@ -214,9 +225,7 @@ $(function() {
     })[0];
 
     let editHTML = editContactScript(contact);
-    showForm(editHTML);
+    ui.showForm(editHTML);
   });
-
-
 });
 
